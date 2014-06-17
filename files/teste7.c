423efa3d84224b9f89201c829d4a77235122aeda
Nuno <numicola@gmail.com>
2014-06-17 16:33:56 +0100

    coiso

--- files/teste7.c
+++ files/teste7.c
@@ -1,10 +1,10 @@
 
 pre: n >= 0 && r == 1;
-b=(r-1)*(r-1);
+
 while (r*r < n){
-inv: n >= 0 && (r-1)*(r-1) <= n;
+inv: n >= 0 && n >= (r-1)*(r-1);
 r = r+1;}
 if(r * r > n)
 {r = r-1;}
 else{}
-pos: r*r <= n && r+1*r+1 > n;
+pos: r*r <= n && n < (r+1)*(r+1);
